35bae0ba06e27e016d3552faef5465a6f9e7ab74
Paulo Oyama <papaloyama@gmail.com>
2023-10-31 09:52:29 -0300

    Trying to implement Naive Bayes #3

--- Naives Bayes/Naive_Bayes_spam.R
+++ Naives Bayes/Naive_Bayes_spam.R
@@ -4,16 +4,20 @@ df = read.csv("data/spam_or_not_spam.csv")
 
 set.seed(2)
 
+#Dividing in train an test (70,30)
 sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
 train  <- df[sample, ]
 test   <- df[!sample, ]
 
+#Take the number of spam and non spam
 spam = length(train$label[train$label != 0])
 not_spam = length(train$label) - spam
 
+#Take the probability of be spam and non spam in relational with the total number
+p_spam = spam/length(train$label)
+p_not_spam = not_spam/length(train$label)
 
 #Time to code
-
 df2 <- data.frame(words=c(""),spam=c(0),ham=c(0))
 for (i in 1:nrow(train)){
   
@@ -48,15 +52,17 @@ for (i in 1:nrow(train)){
   
   #Maybe remove "" and NUMBER string ? 
 }
-rm(i)
+
+
 for(i in 1:nrow(df2)){
-  df2$spam[i] = df2$spam[i]/spam
-  df2$ham[i] = df2$ham[i]/not_spam
+  df2$p_spam[i] = df2$spam[i]/spam
+  df2$p_ham[i] = df2$ham[i]/not_spam
 }
 
 
 for(i in 1:nrow(test)){
-  
+  rm(probability_spam)
+  rm(probability_ham)
   #find duplicated values
   words_dup = duplicated(strsplit(test[i,1],"[[:space:]]")[[1]])
   
@@ -65,26 +71,30 @@ for(i in 1:nrow(test)){
   
 #spam
 probability_spam = 1
-for (i in 1:nrow(df2)){
-  for (j in 1:length(words)){
-    if(words[j] %in% df2$words){
-      probability_spam = probability_spam * df2$spam[df2$words == words[j]]
-    }else{
-      probability_spam = probability_spam * (1 - df2$spam[df2$words == words[j]])
-    }
+for (j in 1:length(words)){
+  wd = df2$p_spam[df2$words == words[j]]
+  if(words[j] %in% df2$words && wd != 0){
+    probability_spam = probability_spam * log(df2$p_spam[df2$words == words[j]])
+  }else{
+    next
   }
 }
 
 
 #ham
 probability_ham = 1
-for (i in 1:nrow(df2)){
-  for (j in 1:length(words)){
-    if(words[j] %in% df2$words){
-      probability_ham = probability_ham * df2$spam[j]
-    }else{
-      probability_ham = probability_ham * (1 - df2$spam[j])
-    }
+for (j in 1:length(words)){
+  wd = df2$p_ham[df2$words == words[j]]
+  if(words[j] %in% df2$words && wd != 0){
+    probability_ham = probability_ham * log(df2$p_ham[df2$words == words[j]])
+  }else{
+    next
   }
 }
+upper <- probability_spam*log(p_spam)
+lower <- probability_ham*log(p_not_spam)
+test$prob[i] <-  ifelse(upper/lower > 0.80, 1, 0)
+
 }
+
+
